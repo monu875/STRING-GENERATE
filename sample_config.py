1d0bef07e041ff76c504ce034380f1d6d9b6bdb4
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-06-04 09:46:59 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 ENV = os.environ.get("ENV", False)
 try:
-    API_ID = int(os.environ.get("API_ID", 20986895))
+    API_ID = int(os.environ.get("API_ID", "20986895"))
 except ValueError:
     raise Exception(
         "Your API_ID is not a valid integer.\nPlease Check The APP_ID Correctly"
     )
-API_HASH = os.environ.get("API_HASH", d81ceab1b76c91610efb2e2b016d0e04)
+API_HASH = os.environ.get("API_HASH", "d81ceab1b76c91610efb2e2b016d0e04")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
